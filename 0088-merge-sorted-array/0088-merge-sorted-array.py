e8361cd1ca06a92625810ce3b8ced8826cd72fae
Okitta Ongaye <88825383+okitta@users.noreply.github.com>
2022-12-24 09:27:47 +0300

    Time: 36 ms (94.45%), Space: 13.9 MB (86.42%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -10,10 +10,7 @@ class Solution:
         for iterator in range( len(nums1)-1 , -1 , -1):
             if pointer_num2 < 0:
                 break
-            elif pointer_num1 < 0:
-                nums1[iterator] = nums2[pointer_num2]
-                pointer_num2 -=1
-            elif  nums1[pointer_num1] < nums2[pointer_num2]: 
+            elif pointer_num1 < 0 or  nums1[pointer_num1] < nums2[pointer_num2]: 
                 nums1[iterator] = nums2[pointer_num2]
                 pointer_num2 -= 1
             else:
