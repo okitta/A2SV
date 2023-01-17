c4f3f7a04c0c233082177a791db274f6aa64485f
Okitta Ongaye <88825383+okitta@users.noreply.github.com>
2023-01-17 18:48:52 +0300

    Time: 583 ms (10.59%), Space: 14.3 MB (84.60%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,13 +1,12 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         for left in range(len(names)):
-            minimum = heights[left]
-            min_index = left
+            max_index = left
             for right in range(left+1,len(names)):
-                if minimum < heights[right]:
-                    min_index = right
-                    minimum = heights[right]
-            names[min_index],names[left] = names[left],names[min_index]
-            heights[min_index],heights[left] = heights[left],heights[min_index]
+                if heights[max_index] < heights[right]:
+                    max_index = right
+                    heights[max_index] = heights[right]
+            names[max_index],names[left] = names[left],names[max_index]
+            heights[max_index],heights[left] = heights[left],heights[max_index]
         return names
         
