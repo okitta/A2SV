5246dd0f70bb676c2a42bb186eef28fe2e37c94b
Okitta Ongaye <88825383+okitta@users.noreply.github.com>
2023-01-17 18:45:15 +0300

    Time: 489 ms (12.26%), Space: 14.2 MB (99.41%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,20 +1,13 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         for left in range(len(names)):
-            for right in range(len(names)):
-                if heights[left] > heights[right]:
-                    names[left],names[right] = names[right],names[left]
-                    heights[left],heights[right] = heights[right],heights[left]
+            minimum = heights[left]
+            min_index = left
+            for right in range(left+1,len(names)):
+                if minimum < heights[right]:
+                    min_index = right
+                    minimum = heights[right]
+            names[min_index],names[left] = names[left],names[min_index]
+            heights[min_index],heights[left] = heights[left],heights[min_index]
         return names
-#         for item in range(len(heights)):
-#             left_pointer = 0
-#             right_pointer = 1
-#             while right_pointer < len(heights):
-#                 if heights[left_pointer] < heights[right_pointer]:
-#                     names[left_pointer],names[right_pointer] = names[right_pointer],names[left_pointer]
-#                     heights[left_pointer],heights[right_pointer] = heights[right_pointer],heights[left_pointer]
-#                 right_pointer +=1
-#                 left_pointer +=1
-            
-#         return names 
         
