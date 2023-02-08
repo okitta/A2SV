6dcb60619fa2a93065a38e491b74b94aa8a5a1bc
Okitta Ongaye <88825383+okitta@users.noreply.github.com>
2023-02-08 18:21:54 +0300

    Time: 602 ms (46.90%), Space: 27.8 MB (83.45%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
@@ -5,25 +5,13 @@ class Solution:
         left_pointer =1 
         right_pointer = size-2
         check_num = skill[0] + skill[right_pointer+1]
-        check = []
-        check.append(skill[0])
-        check.append(skill[right_pointer+1])
+        pro = skill[0] * skill[right_pointer+1]
         while left_pointer <= right_pointer:
             num = skill[left_pointer] + skill[right_pointer]
             if num == check_num:
-                check.append(skill[left_pointer])
-                check.append(skill[right_pointer])
+                pro = pro + (skill[left_pointer] * skill[right_pointer])
                 right_pointer -= 1
                 left_pointer += 1
             else:
                 return -1
-        # print(check)
-        pro = 0
-        right = 1
-        left = 0
-        while right < len(check):
-            pro = pro + (check[left]*check[right])
-            # print(pro)
-            left += 2
-            right = left+1
         return pro
