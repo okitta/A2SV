b04aaff4a8c3d3aeef20026019e10390c56b12de
Okitta Ongaye <88825383+okitta@users.noreply.github.com>
2023-02-27 14:23:56 +0300

    Time: 69 ms (90.74%), Space: 14.3 MB (80.82%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,17 +1,29 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        trips.sort(key = lambda trip:trip[1])
-        minHeap = []
-        currPassenger = 0
-        
+        arr = [0] * 1001
         for trip in trips:
-            numPassenger ,start, end = trip
-            
-            while minHeap and minHeap[0][0] <= start:
-                currPassenger -= minHeap[0][1]
-                heapq.heappop(minHeap)
-            currPassenger += numPassenger
-            if currPassenger > capacity:
+            numPass,start,end = trip
+            arr[start] += numPass
+            arr[end] -= numPass
+        currPass = 0
+        for item in arr:
+            currPass += item
+            if currPass > capacity:
                 return False
-            heapq.heappush(minHeap,[end,numPassenger])
         return True
+        
+#         trips.sort(key = lambda trip:trip[1])
+#         minHeap = []
+#         currPassenger = 0
+        
+#         for trip in trips:
+#             numPassenger ,start, end = trip
+            
+#             while minHeap and minHeap[0][0] <= start:
+#                 currPassenger -= minHeap[0][1]
+#                 heapq.heappop(minHeap)
+#             currPassenger += numPassenger
+#             if currPassenger > capacity:
+#                 return False
+#             heapq.heappush(minHeap,[end,numPassenger])
+#         return True
