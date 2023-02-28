b1115bd4bdf093182870cf55344870aac26b3a5d
Okitta Ongaye <88825383+okitta@users.noreply.github.com>
2023-02-28 18:31:32 +0300

    Time: 914 ms (47.38%), Space: 22.4 MB (19.48%) - LeetHub

---
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        pref=list(accumulate([0 if num%2==0 else 1 for num in nums]))
+        print(pref)
+        dico=defaultdict(int)
+        res=0
+        dico[0]=1
+        for num in pref:
+            if num-k in dico:
+                res+=dico[num-k]
+            dico[num]+=1
+        return res
+        
