7cfbf77e0794234c58f7f85153ecc827146f57c9
Okitta Ongaye <88825383+okitta@users.noreply.github.com>
2023-07-09 00:11:10 +0300

    added is-subsequence.python

---
+++ is-subsequence.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        pointer1 = pointer2 = counter = 0
+        # ans = False
+        while pointer1 < len(s) and pointer2 < len(t):
+            if s[pointer1] == t[pointer2]:
+                counter += 1
+                pointer1 += 1
+                pointer2 += 1
+            else:
+                pointer2 += 1
+        if counter == len(s):
+            return True
+        return False
