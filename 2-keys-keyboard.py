d7af3436c358cadc20a94aaf48931c9e940b4b99
Okitta Ongaye <88825383+okitta@users.noreply.github.com>
2023-09-18 14:46:40 +0300

    added 2-keys-keyboard.python

--- 2-keys-keyboard.py
+++ 2-keys-keyboard.py
@@ -1,16 +1,18 @@
 class Solution:
     def minSteps(self, n: int) -> int:
-        dictionary = {}
-        def helper(screen, clipboard):
-            if (screen, clipboard) in dictionary: return dictionary[(screen, clipboard)]
-            if screen == n: return 0
-            if screen > n: return float("Inf")
-            
-            copy_paste = helper(screen+screen, screen) + 2
-            paste = float("Inf")
-            if clipboard:
-                paste = helper(screen + clipboard, clipboard) + 1
+        dict_char_num = {}
 
-            dictionary[(screen, clipboard)] = min(copy_paste, paste)    
-            return dictionary[(screen, clipboard)]
-        return helper(1, 0)
+        def dfs(num,chart):
+            if (num,chart) in dict_char_num: return dict_char_num[(num,chart)]
+            if num == n: return 0
+            if num > n: return float("Inf")
+
+            copy = dfs(num+num,num) + 2
+            paste = float("Inf")
+            if chart:
+                paste = dfs(num+chart,chart) + 1
+            
+            dict_char_num[(num,chart)] = min(copy,paste)
+            return dict_char_num[(num,chart)]
+        
+        return dfs(1,0)
