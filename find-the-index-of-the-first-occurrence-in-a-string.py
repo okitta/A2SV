fe1a018ed540517bc495357cec6940b421a3790c
Okitta Ongaye <88825383+okitta@users.noreply.github.com>
2023-10-11 18:24:57 +0300

    added find-the-index-of-the-first-occurrence-in-a-string.python

--- find-the-index-of-the-first-occurrence-in-a-string.py
+++ find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,15 +1,25 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        pointer1 = pointer2 = 0
-        while pointer1 < len(haystack) and pointer2 < len(needle):
-            if pointer2 == len(needle):
-                return pointer1 - len(needle)
-            if haystack[pointer1] == needle[pointer2]:
-                pointer2 += 1
-            else:
-                pointer1 = pointer1- pointer2
-                pointer2 = 0
-            pointer1 += 1
-        if pointer2 == len(needle):
-            return pointer1 - len(needle)
+        
+        if len(haystack) < len(needle):
+            return -1
+        window = len(needle)
+        need = 0
+        look = 0
+        # flag = False
+        for i in range(window):
+            need += ((26**(window-i-1))*((ord(needle[i])-ord('a'))+1))
+            look += ((26**(window-i-1))*(ord(haystack[i])-ord('a')+1))
+        if look == need:
+            return 0
+        for i in range(window,len(haystack)):
+
+            look -= ((ord(haystack[i-window])-ord('a')+1)*(26**(window-1)))
+            look*= 26
+            look += (ord(haystack[i])-ord('a')+1)
+            # print(look,need,i)
+            # if len(needle) == 1 and look==need:
+            #     return i+1
+            if look == need:
+                return (i-window)+1
         return -1
