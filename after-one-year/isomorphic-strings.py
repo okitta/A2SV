671a0a58aa625a7bf8ae90aeac7039675f90d86b
Okitta Ongaye <88825383+okitta@users.noreply.github.com>
2023-11-17 21:24:42 +0300

    Added solution for Isomorphic Strings

---
+++ after-one-year/isomorphic-strings.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        map1 = []
+        map2 = []
+        for idx in s:
+            map1.append(s.index(idx))
+        for idx in t:
+            map2.append(t.index(idx))
+        # print(map1,map2)
+        return map1 == map2
